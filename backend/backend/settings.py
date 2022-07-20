f5ccc95317427831337de5adfa22f288ee26d6a5
Roman Korneev <rm@ipadla.org>
2022-07-21 00:03:55 +0300

    Removed unused backend settings and tests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,12 +108,3 @@ REST_FRAMEWORK = {
 DJOSER = {
     'LOGIN_FIELD': 'email',
 }
-
-if os.getenv('PYTEST', default=False):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'memory:',
-        }
-    }
-    EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
--- backend/backend/tests/test_settings.py
+++ backend/backend/tests/test_settings.py
@@ -2,14 +2,6 @@ from django.conf import settings
 
 
 class TestSettings:
-    def test_pytest_overriden_settings(self):
-        assert settings.DATABASES["default"]["ENGINE"] == "django.db.backends.sqlite3"
-        assert (
-            settings.DATABASES["default"]["NAME"]
-            == "file:memorydb_default?mode=memory&cache=shared"
-        )
-        assert settings.EMAIL_BACKEND == "django.core.mail.backends.locmem.EmailBackend"
-
     def test_djoser_settings(self):
         assert settings.DJOSER["LOGIN_FIELD"] == "email"
 
