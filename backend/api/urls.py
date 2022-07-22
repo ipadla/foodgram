307b0eba0c07fc8c5fb81a23fa3ba86bc0c7fa56
Roman Korneev <rm@ipadla.org>
2022-07-22 21:04:03 +0300

    Users: urls moved from backend to api app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 app_name = 'api'
 
 urlpatterns = [
+    path('', include('users.urls', namespace='users')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
