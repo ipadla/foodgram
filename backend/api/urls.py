d812716d8882ba448940e5dc242935504c4b69ae
Roman Korneev <rm@ipadla.org>
2022-07-26 23:14:09 +0300

    Recipes urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 app_name = 'api'
 
 urlpatterns = [
+    path('', include('recipes.urls', namespace='recipes')),
     path('', include('tags.urls', namespace='tags')),
     path('', include('users.urls', namespace='users')),
     path('auth/', include('djoser.urls.authtoken'))
